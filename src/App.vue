783e760f7805c660077409ee28c1b90dc1b9cb91
monkeymoves <lukejjmaggs@gmail.com>
2021-09-26 15:56:05 +0100

    1 binaries uploaded

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
+    <img alt="Vue logo" src="LogoD3_white.ico">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
